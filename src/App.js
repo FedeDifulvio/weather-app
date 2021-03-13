63dcb184d141eb4fb0c21752adfd9a902f8753f3
FedeDifulvio <71615110+FedeDifulvio@users.noreply.github.com>
2021-03-13 15:19:54 -0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Principal from './Components/principal';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 import ExtendedWeather from './Components/extendedWeather';
 
 function App() { 
 
   return (
     <Router>
-    <h1>holi</h1>
       <Switch>
         <Route exact path="/">
           <Principal/>
